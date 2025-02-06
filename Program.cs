2299260eb09f870f1b377ca375dd3b3bb2a18905
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-02-06 21:41:50 +0200

    Aggeregation Operators - Immediate Execution

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static LINQ_02.ListGenerator;
+
+namespace LINQ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Aggeregation Operators - Immediate Execution
+
+            #region Count() , TryGetNonEnumeratedCount()
+
+            //  var Count = ProductList.Count();
+            //  Count = ProductList.Count;
+            //  ProductList.TryGetNonEnumeratedCount(out Count);
+            //  
+            //  IEnumerable<int> Numbers = Enumerable.Range(1, 100); // 1 .. 100
+            //  Count = Numbers.Count();
+            //
+            //  Count = ProductList.Where( p => p.UnitsInStock == 0).Count();
+            //  Count = ProductLisat.Count(P => P.UnitsInStock == 0);
+            //
+            //  Count = ProductList.Where(p => p.UnitsInStock == 0).TryGetNonEnumeratedCount(out Count); //0
+            //
+            //  Console.WriteLine(Count);
+            #endregion
+
+            #region Sum() , Average()
+
+            // var Result = ProductList.Sum(P => P.UnitPrice);
+            // 
+            // Result =
+
+
+            #endregion
+
+            #region Max() , Min() [ First 2 Overloads] , MaxBy() , MinBy() [ .NET 6.0 Feature]
+            //       var MaxProduct = ProductList.Max();
+            //   MaxProduct = ProductList.Max( new ProductComparer() );
+            //   MaxProduct = ProductList.OrderByDescending( p => p.UnitPrice).FirstOrDefault();
+            //
+            //   MaxProduct = ProductList.MaxBy(p => p.UnitPrice);
+            //   MaxProduct = ProductList.MaxBy(p => p.UnitsInStock);
+            //   MaxProduct = ProductList.MaxBy(p => p.UnitsInStock , new IntComparer());
+            //
+            //
+            //   var MinProduct = ProductList.Min();
+            //   MinProduct = ProductList.Min(new ProductComparer() );
+            //   MinProduct = ProductList.OrderByDescending(p => p.UnitPrice).FirstOrDefault();
+            //   Console.WriteLine(MaxProduct);
+            #endregion
+
+            #region Max() , Min() [ Other Overloads] 
+            //   var MaxPrice = ProductList.Max(P => P.UnitPrice);
+            //
+            //   var Result = ProductList.Max(P => P.ProductName);
+            //
+            //   var MinPrice = ProductList.Max(P => P.UnitPrice);
+            //
+            //   Result = ProductList.Max(P => P.ProductName);
+            //
+            //   Console.WriteLine(Result);
+            #endregion
+
+            #region Aggregate()
+            //
+            // string[] Name = ["Bassem", "Amin", "Doaa", "Ahmed"];
+            //
+            // string FullName = Name.Aggregate((str01 , str02) => $"{str01} , {str02}");
+            // FullName = Name.Aggregate("Hello", (str01, str02) => $"{str01} , {str02}");
+            // FullName = Name.Aggregate("Hello", (str01, str02) => $"{str01} , {str02}", (TACum) => TACum.Replace(' ' , '$'));
+            //
+            //
+            // Console.WriteLine(FullName);
+
+            #endregion
+
+            #endregion
+        }
+    }
+    }
